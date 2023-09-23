0d7225e5f876d03bdba3b2ae26b063daece6132e
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2023-09-23 23:54:39 +0900

    Update 20230923_2166.cpp

--- 20230923_2166.cpp
+++ 20230923_2166.cpp
@@ -15,13 +15,16 @@ void input(){
     for(int i=0;i<n;i++) cin>>p[i].x>>p[i].y;
 }
 //면적구하기
-double Area(){
+void Area(){
     //면적 구하기
     double surface = 0.0;
     for (int i = 1; i < n; i++){
-        surface += ((p[i-1].x - p[0].x)*(p[i].y - p[0].y) - (p[i-1].y - p[0].y)*(p[i].x - p[0].x)) / 2;
+        surface += ((p[i-1].x - p[0].x)*(p[i].y - p[0].y) - (p[i-1].y - p[0].y)*(p[i].x - p[0].x))/2;
     }
-    surface*=surface<0?-1:1;
+    surface*=surface<0?-1:1;//면적은 양수
+    //소수점 첫째자리까지 출력
+    cout<<fixed;
+    cout.precision(1);
     cout<<surface;
 }
 int main()
